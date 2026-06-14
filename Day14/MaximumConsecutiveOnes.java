d6abeea59a6dca4f351fcb0999dc16655f86a805
Abhishek Tiwari <97331593+iamabhish23@users.noreply.github.com>
2026-06-15 00:00:58 +0530

    Create MaximumConsecutiveOnes.java

---
+++ Day14/MaximumConsecutiveOnes.java
@@ -0,0 +1,25 @@
+/*
+Question: Maximum Consecutive Ones
+
+Time: O(n)
+Space: O(1)
+*/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int count = 0;
+        int maxCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
